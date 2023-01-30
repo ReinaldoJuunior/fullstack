413ecda6b13657fb7683b72e39cd27189f6db2ef
ReinaldoJuunior <gamers.jrxiii@gmail.com>
2023-01-30 17:39:44 -0300

    creating splash screen

---
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Meu Header</h1>
+</template>
+<script>
+export default {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'animate.css'
-import '@/assets/css/tailwild.css'
-import '@/assets/css/fonts.css'
 
-const App = createApp(App)
-App.use(router)
-App.mount('#app')
+const app = createApp(App);
+app.use(router)
+app.mount('#app')
